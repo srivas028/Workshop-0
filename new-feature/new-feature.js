732ef34ce243a26c585f8319e770a27cf9e4cdd3
srivas028 <88985855+srivas028@users.noreply.github.com>
2022-03-08 16:19:00 +0530

    Update new-feature.js

--- new-feature/new-feature.js
+++ new-feature/new-feature.js
@@ -1,18 +1,1 @@
 
-let a = 10;
-let b = 20;
-
-const multiply = (a, b) => {
-    console.log(a * b);
-};
-
-multiply(a, b);
-
-let a = 105;
-let b = 30;
-
-const Division = (a, b) => {
-    console.log(a / b);
-};
-
-Division(a, b);
